24678c327b20c99773838ba292ab8f200500479b
Aleksei Krasikov <krasikov.as@phystech.edu>
2018-08-12 18:08:17 +0300

    Patch appr_steiner (RSP): name attr. issue

--- R/steinertree.R
+++ R/steinertree.R
@@ -30,35 +30,42 @@ asp_steiner <- function (optimize, terminals, glist, color) {
 
 
 
+# Randomized all shortest paths approximation (RSP)
 appr_steiner <- function (repeattimes, optimize, terminals, glist, color) {
         set <- c()
         g   <- glist[[1]]
-    
+        
+        # Start with the sub-graph G* consisting of all nodes and edges appearing on shortest paths between terminals
         paths <- lapply(terminals, function (x) get.all.shortest.paths(g, x, terminals)$res)
         
         r  <- 1:length(paths)
         t1 <- lapply(r, function (r) length(paths[[r]]))
-        distances <- lapply(r, function (r) lapply(1:t1[[r]], function(x, y) length(paths[[y]][[x]]), y = r))
         
+        distances <- lapply(r, function (r) lapply(1:t1[[r]], function(x, y) length(paths[[y]][[x]]), y = r))
         neighbour_distance <- max(unlist(distances))
         
-        paths <- unique(unlist(paths))
-        set   <- V(g)[paths]
+        # Note, graph has to have name attribute, because we assign names of vertices to
+        # path variable. It is much more convenient to work with names, not with ids.
+        #paths <- unique(unlist(paths))
+        paths <- unique(names(unlist(paths)))
+        #set   <- V(g)[paths]
+        set   <- V(g)[paths]$name
         size  <- length(E(minimum.spanning.tree(induced_subgraph(g, union(terminals, set)))))
-    
+        
         i <- 1
         while (i <= repeattimes) {
-                seed_list <- unlist(neighborhood(graph = g, order = neighbour_distance, nodes = terminals, mode = "all"))
+        	#seed_list <- unlist(neighborhood(graph = g, order = neighbour_distance, nodes = terminals, mode = "all"))
+                seed_list <- names(unlist(neighborhood(graph = g, order = neighbour_distance, nodes = terminals, mode = "all")))
                 seed_list <- seed_list[!(seed_list %in% terminals)]
-            
                 seed <- sample(seed_list, 1)
             
                 paths2 <- get.all.shortest.paths(g, seed, terminals)
                 paths2 <- paths2$res
 
-                seedpaths <- unique(unlist(paths2))
+                #seedpaths <- unique(unlist(paths2))
+                seedpaths <- unique(names(unlist(paths2)))
                         
-                set2  <- union(set, V(g)[seedpaths])
+                set2  <- union(set, V(g)[seedpaths]$name)
                 size2 <- length(E(minimum.spanning.tree(induced_subgraph(g, union(terminals, set2)))))
                         
                 if (size2 < size) {
@@ -67,7 +74,7 @@ appr_steiner <- function (repeattimes, optimize, terminals, glist, color) {
                 }
                         
                 seed  <- sample(set, 1, prob = NULL)
-                set2  <- V(g)[setdiff(set, seed)]
+                set2  <- V(g)[setdiff(set, seed)]$name
                 size2 <- length(E(minimum.spanning.tree(induced_subgraph(g, union(terminals, set2)))))
             
                 if (size2  < size && is.connected(minimum.spanning.tree(induced_subgraph(g, union(terminals, set2))))) {
@@ -77,7 +84,8 @@ appr_steiner <- function (repeattimes, optimize, terminals, glist, color) {
                         
                 i <- i + 1
         }
-    
+        
+        # Perform "optimization": find minimum spanning tree and remove nodes of degree 1
         if (optimize) {
                 steinert <- minimum.spanning.tree(induced_subgraph(g, union(terminals, set)))
                 a     <- V(steinert)$color
@@ -92,7 +100,7 @@ appr_steiner <- function (repeattimes, optimize, terminals, glist, color) {
     
         glst <- c()
         if (color) {
-                V(g)[setdiff(set, as.numeric(terminals))]$color <- "green"
+                V(g)[setdiff(set, terminals)]$color <- "green"
                 glst[[length(glst) + 1]]  <- g
         }
     
@@ -102,9 +110,10 @@ appr_steiner <- function (repeattimes, optimize, terminals, glist, color) {
 }
 
 
+
 # Shortest Path Based Approximation
 steinertree2 <- function (optimize, terminals, glist, color) {
-        g     <- glist[[1]]
+        g <- glist[[1]]
         
         # Pick a terminal randomly and Form a subtree (sub-graph G')
         prob     <- sample(1:length(terminals), 1)
@@ -180,6 +189,7 @@ steinertree2 <- function (optimize, terminals, glist, color) {
     
         return(glst)
 }
+
 
 
 # Minimum spanning tree based approximation (Kruskal's minimum spanning tree algorithm)
